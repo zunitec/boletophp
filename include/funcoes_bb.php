79a787637a6d8160c9e747d6cc39715774ca4df6
Zuni-Tec Tecnologias <Zuni-Tec Tecnologias@192.168.1.50>
2014-04-29 14:19:32 -0300

    alteração para o caminho das imagens

--- include/funcoes_bb.php
+++ include/funcoes_bb.php
@@ -160,10 +160,10 @@ $altura = 50 ;
 
 
 //Guarda inicial
-?><img src=../../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/p.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
-src=../../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/b.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
-src=../../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/p.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
-src=../../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/b.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
+?><img src=../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/p.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
+src=../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/b.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
+src=../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/p.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
+src=../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/b.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
 <?php
 $texto = $valor ;
 if((strlen($texto) % 2) <> 0){
@@ -182,7 +182,7 @@ while (strlen($texto) > 0) {
       $f1 = $largo ;
     }
 ?>
-    src=../../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/p.png width=<?php echo $f1?> height=<?php echo $altura?> border=0><img 
+    src=../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/p.png width=<?php echo $f1?> height=<?php echo $altura?> border=0><img 
 <?php
     if (substr($f,$i,1) == "0") {
       $f2 = $fino ;
@@ -190,16 +190,16 @@ while (strlen($texto) > 0) {
       $f2 = $largo ;
     }
 ?>
-    src=../../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/b.png width=<?php echo $f2?> height=<?php echo $altura?> border=0><img 
+    src=../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/b.png width=<?php echo $f2?> height=<?php echo $altura?> border=0><img 
 <?php
   }
 }
 
 // Draw guarda final
 ?>
-src=../../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/p.png width=<?php echo $largo?> height=<?php echo $altura?> border=0><img 
-src=../../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/b.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
-src=../../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/p.png width=<?php echo 1?> height=<?php echo $altura?> border=0> 
+src=../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/p.png width=<?php echo $largo?> height=<?php echo $altura?> border=0><img 
+src=../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/b.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
+src=../../../vendor/zuni/boletophp/Zuni/Boleto/imagens/p.png width=<?php echo 1?> height=<?php echo $altura?> border=0> 
   <?php
 } //Fim da fun��o
 
